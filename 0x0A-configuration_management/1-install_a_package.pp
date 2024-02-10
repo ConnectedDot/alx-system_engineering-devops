8c4a649fe87789c1ad837a68c06ca133b3dfc26d
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2024-02-10 20:23:21 +0100

    Fixing

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,13 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+
+class main {
+  package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+  }
+}
+
+include main
