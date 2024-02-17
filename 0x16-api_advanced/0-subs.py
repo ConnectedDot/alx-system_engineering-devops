72381fb7dead6084133bcb89524bfdec7d543de5
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2024-02-17 08:06:57 +0100

    Added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+
+
+"""This script queries the Reddit API and returns
+the number of total subcribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """Returns the number of subscribers for a given subreddit."""
+    if type(subreddit) is not str:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
+
+    response = requests.get(url, headers=user_agent)
+    try:
+        return (response.json()["data"]["subscribers"])
+    except Exception:
+        return 0
